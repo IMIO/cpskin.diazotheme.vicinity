5f22baa1fac5d2c88cf29acb9871a5b3421afbc4
Laurent Lasudry <lasudry@gmail.com>
2022-01-20 11:24:13 +0100

    Back to development: 0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.vicinity package."""
 
-version = '0.5'
+version = '0.6.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
